e4535f41c163300ab48713a76c959386de390f64
siwonKH <kanghyunkim313@gmail.com>
2022-06-28 11:02:14 +0900

    error response change

--- app.js
+++ app.js
@@ -71,11 +71,8 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-    res.locals.message = err.message;
-    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
-    res.status(err.status || 500);
-    res.render('error');
-});
+    res.json({success: false, message: "오류"})
+})
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
